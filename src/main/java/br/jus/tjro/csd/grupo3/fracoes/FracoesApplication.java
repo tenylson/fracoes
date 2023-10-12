60c8b37789529bbc55b074bd48fa80efc3b9a441
Luciano Tenylson <tenylson@tjro.jus.br>
2023-10-12 13:31:00 -0400

    Commit inicial

---
+++ src/main/java/br/jus/tjro/csd/grupo3/fracoes/FracoesApplication.java
@@ -0,0 +1,13 @@
+package br.jus.tjro.csd.grupo3.fracoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FracoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FracoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/jus/tjro/csd/grupo3/fracoes/FracoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.tjro.csd.grupo3.fracoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FracoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
